547bc45f6459c0dc1703c45edda6f9f5d2a3a868
Muhammed-walied <74426146+MohamedWalied-dash@users.noreply.github.com>
2023-09-25 23:12:47 +0300

    edit home screen and search screen

--- Src/Screens/Home.js
+++ Src/Screens/Home.js
@@ -1,12 +1,116 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import Header from "../Components/Header";
+import { COLORS } from "../Conts/COLORS";
+import Banner from "../Components/Banner";
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList } from "react-native";
+import MovieCard from "../Components/MovieCard";
+import { Entypo } from "@expo/vector-icons";
+const MovieData = [
+  {
+    name: "Harry Potter 1",
+    id: 1,
+    image: require("../../assets/download.jpeg"),
+    rate: 5,
+  },
+  {
+    name: "Harry Potter 2",
+    id: 2,
+    image: require("../../assets/download.jpeg"),
+    rate: 5,
+  },
+  {
+    name: "Harry Potter 3",
+    id: 3,
+    image: require("../../assets/download.jpeg"),
+    rate: 5,
+  },
+  {
+    name: "Harry Potter 4",
+    id: 4,
+    image: require("../../assets/download.jpeg"),
+    rate: 5,
+  },
+  {
+    name: "Harry Potter 5",
+    id: 5,
+    image: require("../../assets/download.jpeg"),
+    rate: 5,
+  },
+];
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{ flex: 1, paddingTop: 5, backgroundColor: COLORS.backgrond }}
+    >
+      <ScrollView>
+        <Header name={"Walied"} visable />
 
-export default Home
+        <Banner />
+
+        <View
+          style={{
+            padding: 10,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 20, color: COLORS.text }}>Popular</Text>
+          <Text></Text>
+          <TouchableOpacity>
+            <Entypo
+              name="dots-three-horizontal"
+              size={24}
+              color={COLORS.Button}
+            />
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={MovieData}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <MovieCard name={item.name} image={item.image} rate={item.rate} />
+          )}
+        />
+
+        <View
+          style={{
+            padding: 10,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 20, color: COLORS.text }}>You may like</Text>
+          <Text></Text>
+          <TouchableOpacity>
+            <Entypo
+              name="dots-three-horizontal"
+              size={24}
+              color={COLORS.Button}
+            />
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={MovieData}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <MovieCard name={item.name} image={item.image} />
+          )}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
--- Src/Screens/Search.js
+++ Src/Screens/Search.js
@@ -1,12 +1,135 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { COLORS } from "../Conts/COLORS";
+import Header from "../Components/Header";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { TextInput } from "react-native";
+import FavoriteCard from "../Components/FavoriteCard";
+import { FlatList } from "react-native";
+import { ScrollView } from "react-native";
 
-const Search = () => {
+const data = [
+  {
+    image: require("../../assets/download.jpeg"),
+    name: "Harry Potter 1",
+    type: "Eng | Fiction | 2h10m",
+    rate: "4.5",
+    id: 1,
+  },
+  {
+    image: require("../../assets/download.jpeg"),
+    name: "Harry Potter 2",
+    type: "Eng | Fiction | 2h10m",
+    rate: "4.5",
+    id: 2,
+  },
+  {
+    image: require("../../assets/download.jpeg"),
+    name: "Harry Potter 3",
+    type: "Eng | Fiction | 2h10m",
+    rate: "4.5",
+    id: 3,
+  },
+  {
+    image: require("../../assets/download.jpeg"),
+    name: "Harry Potter 4",
+    type: "Eng | Fiction | 2h10m",
+    rate: "4.5",
+    id: 4,
+  },
+  {
+    image: require("../../assets/download.jpeg"),
+    name: "Harry Potter 5",
+    type: "Eng | Fiction | 2h10m",
+    rate: "4.5",
+    id: 5,
+  },
+];
+
+const Search = ({ search }) => {
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{ flex: 1, paddingTop: 5, backgroundColor: COLORS.backgrond }}
+    >
+      <ScrollView>
+        <Header name={"Wailed"} />
+        <View
+          style={{
+            flexDirection: "row",
+            padding: 10,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              height: 50,
+              borderRadius: 20,
+              flexDirection: "row",
+              backgroundColor: "#E5E5E5",
+              alignItems: "center",
+              paddingHorizontal: 10,
+            }}
+          >
+            <TextInput
+              style={{
+                flex: 1,
+                height: 40,
+                backgroundColor: "transparent",
+                borderBottomWidth: 0,
+                fontSize: 20,
+                color: "#000",
+              }}
+              placeholder="Search"
+              placeholderTextColor={COLORS.black}
+              //value={searchQuery}
+            />
+          </View>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              marginLeft: 5,
+              backgroundColor: COLORS.Button,
+              borderRadius: 99,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {search ? (
+              <Icon
+                name="cancel"
+                size={28}
+                color={"white"}
+                // onPress={clearSearch} // Trigger handleSearch when the search icon is clicked
+              />
+            ) : (
+              <Icon
+                name="search"
+                size={28}
+                color={"white"}
+                // onPress={handleSearch} // Trigger handleSearch when the search icon is clicked
+              />
+            )}
+          </View>
+        </View>
+        <View>
+          <FlatList
+            data={data}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <FavoriteCard
+                name={item.name}
+                image={item.image}
+                rate={item.rate}
+                type={item.type}
+              />
+            )}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default Search
+export default Search;
